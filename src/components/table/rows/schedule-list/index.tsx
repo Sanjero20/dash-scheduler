bdeebd3f07c100d652cb567c2d0523468d5e261b
Sanjero20 <gelosantos72@gmail.com>
2024-01-17 00:59:58 +0800

    feat: create input fields sample

--- src/components/table/rows/schedule-list/index.tsx
+++ src/components/table/rows/schedule-list/index.tsx
@@ -1,9 +1,90 @@
-import { Fragment, useEffect } from "react";
-import { SCHEDULES } from "@/constants/initial";
+import { ChangeEvent, Fragment, useReducer } from "react";
+import { SCHEDULES, createSchedulePerTime } from "@/constants/initial";
 import ColumnName from "./names";
+import { ISchedule } from "@/types/api";
+
+type IAction =
+  | { type: "UPDATE_COURSE"; index: number; day: string; value: string }
+  | { type: "UPDATE_SECTION"; index: number; day: string; value: string }
+  | { type: "UPDATE_ROOM"; index: number; day: string; value: string };
+
+const scheduleReducer = (state: ISchedule[], action: IAction) => {
+  switch (action.type) {
+    case "UPDATE_COURSE":
+      return state.map((schedule, index) =>
+        index === action.index
+          ? {
+              ...schedule,
+              schedules: schedule.schedules.map((day) => {
+                if (day.day === action.day) {
+                  return { ...day, course: action.value };
+                }
+                return day;
+              }),
+            }
+          : schedule,
+      );
+
+    case "UPDATE_SECTION":
+      return state.map((schedule, index) =>
+        index === action.index
+          ? {
+              ...schedule,
+              schedules: schedule.schedules.map((day) => {
+                if (day.day === action.day) {
+                  return { ...day, section: action.value };
+                }
+                return day;
+              }),
+            }
+          : schedule,
+      );
+
+    case "UPDATE_ROOM":
+      return state.map((schedule, index) =>
+        index === action.index
+          ? {
+              ...schedule,
+              schedules: schedule.schedules.map((day) => {
+                if (day.day === action.day) {
+                  return { ...day, room: action.value };
+                }
+                return day;
+              }),
+            }
+          : schedule,
+      );
+  }
+};
+
+const initialState = createSchedulePerTime();
 
 function ScheduleList() {
-  useEffect(() => {}, []);
+  const [state, dispatch] = useReducer(scheduleReducer, initialState);
+
+  console.clear();
+  console.table(state[0].schedules[0]);
+
+  const handleCourseInput = (
+    e: ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const { value, name } = e.target;
+    dispatch({ type: "UPDATE_COURSE", index, day: name, value });
+  };
+
+  const handleSectionInput = (
+    e: ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const { value, name } = e.target;
+    dispatch({ type: "UPDATE_SECTION", index, day: name, value });
+  };
+
+  const handleRoomInput = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    const { value, name } = e.target;
+    dispatch({ type: "UPDATE_ROOM", index, day: name, value });
+  };
 
   return (
     <>
@@ -13,24 +94,34 @@ function ScheduleList() {
           <tr className="h-8 text-center">
             <td rowSpan={2}>{schedule}</td>
 
-            <td rowSpan={1}></td>
-            <td rowSpan={2}></td>
+            <td rowSpan={1}>
+              <input
+                type="text"
+                name="monday"
+                value={state[index].schedules[0].course}
+                onChange={(e) => handleCourseInput(e, index)}
+              />
+            </td>
+
+            <td rowSpan={2}>
+              <input
+                type="text"
+                name="monday"
+                value={state[index].schedules[0].room}
+                onChange={(e) => handleRoomInput(e, index)}
+              />
+            </td>
 
             <td rowSpan={1}></td>
             <td rowSpan={2}></td>
-
             <td rowSpan={1}></td>
             <td rowSpan={2}></td>
-
             <td rowSpan={1}></td>
             <td rowSpan={2}></td>
-
             <td rowSpan={1}></td>
             <td rowSpan={2}></td>
-
             <td rowSpan={1}></td>
             <td rowSpan={2}></td>
-
             <td rowSpan={1}></td>
             <td rowSpan={2}></td>
 
@@ -49,8 +140,15 @@ function ScheduleList() {
           </tr>
 
           {/* sections */}
-          <tr className="h-8 text-center">
-            <td></td>
+          <tr className="h-8 w-fit text-center">
+            <td>
+              <input
+                type="text"
+                name="monday"
+                value={state[index].schedules[0].section}
+                onChange={(e) => handleSectionInput(e, index)}
+              />
+            </td>
             <td></td>
             <td></td>
             <td></td>
