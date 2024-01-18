760349f87e79f2fc32821defe6f57fd27faa2619
Sanjero20 <gelosantos72@gmail.com>
2024-01-19 03:35:33 +0800

    initialize class schedule table

--- src/pages/user/class-shedule.tsx
+++ src/pages/user/class-shedule.tsx
@@ -1,9 +1,26 @@
 import MainTable from "@/components/table/root";
+import CollegeInfo from "@/components/table/rows/metadata/college-info";
+import LogoRevision from "@/components/table/rows/metadata/logo-revision";
+import ScheduleInfo from "@/components/table/rows/metadata/schedule-info";
+import TableTitle from "@/components/table/rows/metadata/title";
+import ScheduleHeader from "@/components/table/rows/schedule-header";
 
 function ClassSchedulePage() {
   return (
     <div>
-      <MainTable title="class schedule" />
+      <MainTable>
+        <LogoRevision
+          refNumber="04"
+          effectivityDate="January 3, 2019"
+          revisionNumber="02"
+        />
+
+        <TableTitle title="Class Schedule" />
+        <CollegeInfo />
+        <ScheduleInfo category="Section" dropdown />
+
+        <ScheduleHeader category="RM" />
+      </MainTable>
     </div>
   );
 }
