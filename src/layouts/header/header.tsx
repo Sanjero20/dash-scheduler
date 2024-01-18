af98ebfb7e0b590b44c53b7a65bf57d0feb8195d
Sanjero20 <gelosantos72@gmail.com>
2024-01-18 13:19:25 +0800

    fix: header bg on larger screens
    The header's bg-color has the same size as the container, thus limiting
    its display on some larger screens.

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -11,28 +11,30 @@ const ROUTES = [
 
 function Header() {
   return (
-    <header className="container flex items-center justify-between bg-black/80 py-2">
-      <nav className="gap-2 text-white">
-        {ROUTES.map((route) => (
-          <NavLink
-            key={route.path}
-            to={route.path}
-            className={({ isActive }) =>
-              `${isActive ? "bg-white text-black" : undefined} w-12 rounded-xl px-6 py-3`
-            }
-          >
-            {route.placeholder}
-          </NavLink>
-        ))}
-      </nav>
+    <header className="bg-black/80">
+      <div className="container flex items-center justify-between py-2">
+        <nav className="gap-2 text-white">
+          {ROUTES.map((route) => (
+            <NavLink
+              key={route.path}
+              to={route.path}
+              className={({ isActive }) =>
+                `${isActive ? "bg-white text-black" : undefined} w-12 rounded-xl px-6 py-3`
+              }
+            >
+              {route.placeholder}
+            </NavLink>
+          ))}
+        </nav>
 
-      <div className="flex gap-8">
-        <div className="flex gap-2">
-          <ButtonSave />
-          <ButtonPrint />
+        <div className="flex gap-8">
+          <div className="flex gap-2">
+            <ButtonSave />
+            <ButtonPrint />
+          </div>
+
+          <ButtonLogout />
         </div>
-
-        <ButtonLogout />
       </div>
     </header>
   );
