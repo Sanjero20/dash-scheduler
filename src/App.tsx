cf938ee32b9c7510fe7f1a0e1a9be7eef95a41f7
Sanjero20 <gelosantos72@gmail.com>
2024-01-19 04:51:57 +0800

    feat: add toast component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Toaster } from "./components/ui/toaster";
 import { routes } from "./routes/routes";
 import { RouterProvider } from "react-router-dom";
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <>
+      <RouterProvider router={routes} />;
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
