f084c4c54c5a2af7cd40660a244726996ea3bf0b
Sanjero20 <gelosantos72@gmail.com>
2024-01-20 23:56:21 +0800

    feat: create users

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,59 @@
+import { createUser, getUsers } from "@/services/api/admin";
+import { useEffect, useState } from "react";
+
 function AdminHome() {
-  return <div>Admin home page</div>;
+  const [users, setUsers] = useState([]);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const users = await getUsers();
+      setUsers(users);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleCreateAccount = async () => {
+    const response = await createUser(username, password);
+    console.log(response);
+  };
+
+  return (
+    <div>
+      Admin home page
+      {/* Add user */}
+      <div className="flex w-fit flex-col gap-2 border border-black">
+        <label htmlFor="">
+          username
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="">
+          password
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button onClick={handleCreateAccount}>Add Account</button>
+      </div>
+      {/* Users List */}
+      <p>User List</p>
+      {users && (
+        <>
+          {users.map((user, index) => (
+            <p key={index}>{index}</p>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default AdminHome;
--- src/services/api/admin.ts
+++ src/services/api/admin.ts
@@ -1,11 +1,14 @@
 import { api } from "../config";
 
 export async function getUsers() {
-  const response = await "/users";
+  const response = await api.get("/users");
+  const users = await response.data;
+  return users.rows;
 }
 
 export async function createUser(username: string, password: string) {
   const response = await api.post("/users/register", { username, password });
+  return await response.data;
 }
 
 export async function getFaculties() {
