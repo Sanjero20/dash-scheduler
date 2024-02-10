558db7a59a02374193302e537272d1e4a74e475c
HueHueberry <huby.new1112@gmail.com>
2024-02-10 22:05:26 +0800

    added form for admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,11 +3,13 @@ import AccountForm from "./accounts/account-form";
 import AccountList from "./accounts/account-list";
 import FacultyForm from "./faculty/faculty-form";
 import FacultyList from "./faculty/faculty-list";
+import FormDetails from "./faculty/form-details-form";
 
 function AdminHome() {
   return (
     <div className="flex h-full gap-4">
       <Card className="flex w-1/2 flex-col gap-2 overflow-hidden p-2">
+        <FormDetails />
         <AccountForm />
         <AccountList />
       </Card>
--- src/services/api/form.ts
+++ src/services/api/form.ts
@@ -4,4 +4,8 @@ import { api } from "../config";
 
 export async function getFormDetails(): Promise<IFormDetails> {
   return (await api.get('/form-details')).data;
+};
+
+export async function updateFormDetails(data: IFormDetails) {
+  return (await api.post('/form-details', data)).data;
 }
